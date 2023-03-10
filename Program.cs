afd6f61c340b48ac8ecadd1ed7a49194c71d0ffd
miguel <miguelpositivo02@gmail.com>
2023-03-10 20:14:10 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Cananeia");
+Console.WriteLine("Ilha comprida");
+Console.WriteLine("Iguape");
+Console.WriteLine("Peruibe");
+Console.WriteLine("Itanhaem");
+Console.WriteLine("Mongagua");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Sao Vicente");
+Console.WriteLine("Cubatao");
+Console.WriteLine("Santos");
+Console.WriteLine("Guaruja");
+Console.WriteLine("Bertioga");
